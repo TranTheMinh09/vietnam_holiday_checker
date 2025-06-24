a4f2e8554028fcfc7691a1b9d1ce8aa9d5cdfe0b
TranTheMinh09 <trantheminh25092003@gmail.com>
2025-06-24 17:57:38 +0700

    Khởi tạo gem vietnam_holiday_checker với cấu trúc cơ bản

---
+++ lib/vietnam_holiday_checker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vietnam_holiday_checker/version"
+
+module VietnamHolidayChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vietnam_holiday_checker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module VietnamHolidayChecker
+  VERSION = "0.1.0"
+end
